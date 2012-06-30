67280ec6b0e593d74715aa4adbb41e0496873b9d
Linus Shu <linusshinopy@gmail.com>
2012-06-30 15:32:27 -0400

    removed_facebook

--- gen/com/cs446/purfect_litter/R.java
+++ gen/com/cs446/purfect_litter/R.java
@@ -11,9 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int close=0x7f020000;
-        public static final int facebook_icon=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
         public static final int app_title=0x7f050000;
