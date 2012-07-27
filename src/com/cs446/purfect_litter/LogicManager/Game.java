ee1371fd7a69a2ad4dca54d3f89343914a4acfc0
Marina Zhao <m7zhao@gmail.com>
2012-07-26 21:06:03 -0400

    new files

---
+++ src/com/cs446/purfect_litter/LogicManager/Game.java
@@ -0,0 +1,96 @@
+package com.cs446.purfect_litter.LogicManager;
+
+import java.util.ArrayList;
+
+import com.cs446.purfect_litter.MainActivity;
+import com.cs446.purfect_litter.MainActivity.SessionType;
+import com.cs446.purfect_litter.LogicManager.Player.Pile;
+import com.cs446.purfect_litter.LogicManager.CardManager.CardInstance;
+import com.cs446.purfect_litter.SessionManager.ClientManager;
+import com.cs446.purfect_litter.SessionManager.GameSessionManager;
+import com.cs446.purfect_litter.SessionManager.ServerManager;
+
+public class Game {
+	
+	private MainActivity gameUI;
+	private GameSessionManager gameSessionManager;
+	private GameLogic gameLogic;
+	
+	public Game(MainActivity mainActivity, SessionType sessionType) {
+		gameUI = mainActivity;
+		
+		switch (sessionType) {
+		case HOST:
+			gameSessionManager = new ServerManager(this);
+			break;
+		case CLIENT:
+			gameSessionManager = new ClientManager(this);
+		default:
+			break;
+		}
+	}
+	
+
+	// SETTERS ======================================================
+	
+	public void doNextPhase() {
+		// ...
+		gameLogic.getPhase().nextPhase(gameLogic);
+		gameUI.update();
+	}
+	
+	public void doPickCard(CardInstance whichCard) {
+		gameLogic.getPhase().pickCard(whichCard, gameLogic);
+    	gameUI.update();
+	}
+	
+	public void createLogicForServer(ArrayList<String> playerNames)
+	{
+		gameLogic = new GameLogic(playerNames, this);
+		doSendGameState(gameLogic.getGs());
+		gameLogic.update(gameLogic.getGs());
+	}
+	
+	public void createLogicForClient(GameState newGs, String playerName)
+	{
+		gameLogic = new GameLogic(newGs, playerName, this);
+		gameLogic.update(gameLogic.getGs());
+	}
+	
+	public void doSendGameState(GameState curState)
+	{
+		gameSessionManager.send(curState);
+		gameUI.update();
+	}
+	
+	public void receiveGameState(GameState newGs)
+	{
+		gameLogic.update(newGs);
+		gameUI.update();
+	}
+	// GETTERS ======================================================
+	
+	public boolean isItMyTurn() {
+		return gameLogic.getGs().currentPlayer.equals(gameLogic.getMe());
+	}
+	
+	public String getMyName() {
+		return gameLogic.getMe().getName();
+	}
+	
+	public Integer[] getMyCardPile(Pile whichPile) {
+		return gameLogic.getMe().getImageArray(whichPile);
+	}
+	
+	public CardInstance getMyCard(Pile whichPile, int index) {
+		return gameLogic.getMe().piles.get(whichPile.index).get(index);
+	}
+	
+	public String getCurrentPhase() {
+		return gameLogic.getGs().currentPhase.getName();
+	}
+	
+	public GameState getGameState() {
+		return gameLogic.getGs();
+	}
+}
---
+++ src/com/cs446/purfect_litter/LogicManager/Phases/OOTPhase.java
@@ -0,0 +1,26 @@
+package com.cs446.purfect_litter.LogicManager.Phases;
+
+import com.cs446.purfect_litter.LogicManager.GameLogic;
+import com.cs446.purfect_litter.LogicManager.CardManager.CardInstance;
+
+public class OOTPhase implements AbstractPhase{
+
+	@Override
+	public boolean pickCard(CardInstance chosen, GameLogic wrapper) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init(GameLogic wrapper) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nextPhase(GameLogic wrapper) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
