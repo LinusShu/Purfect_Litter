1faff789c91c5d88c8f26e14ee56175f399554b1
Linus Shu <linusshinopy@gmail.com>
2012-07-27 22:32:35 -0300

    Update src/com/cs446/purfect_litter/SessionManager/ClientManager.java

--- src/com/cs446/purfect_litter/SessionManager/ClientManager.java
+++ src/com/cs446/purfect_litter/SessionManager/ClientManager.java
@@ -10,7 +10,9 @@ import com.cs446.purfect_litter.LogicManager.Game;
 import com.cs446.purfect_litter.LogicManager.GameState;
 
 
-
+/**
+ *  Responsible for managing the client side communication tasks.
+ */
 public class ClientManager extends GameSessionManager {
 	Socket outGoingSocket = null;
 	ClientCommsTask ct;
@@ -18,16 +20,21 @@ public class ClientManager extends GameSessionManager {
 	boolean serverStatus;
 	Game G;
 	
+	/**
+	 *  Constructs the ClientManager and set up sockect communication with the server
+	 */
 	public ClientManager(Game G) {
 		this.G = G;
 		
 		try {
+			//TODO: allow the user to enter the server ip address
+			//NOTE: hard-coded server ip address for testing on Android emulators
 			outGoingSocket = new Socket(InetAddress.getByName("10.0.2.2"), 4000);
 			this.init();
 			serverStatus = true;
 		} catch (UnknownHostException e1) {
 			e1.printStackTrace();
-		    serverStatus = false;
+		    	serverStatus = false;
 		} catch (IOException e2) {
 			e2.printStackTrace();
 			serverStatus = false;
@@ -35,49 +42,58 @@ public class ClientManager extends GameSessionManager {
 		
 	}
 	
-	// check if the game state is indeed from the server
+	/**
+	 * Checks if the GameState is indeed from the server
+	 */
 	public boolean validateGameState (GameState g) {
 		return (g.getID() == this.id); 
 	}
 	
-	// return the id of this client
+	/**
+	 * Returns the id of this client
+	 */
 	public int getId() {
 		return this.id;
 	}
 	
-	// return the server status
+	/**
+	 * Returns the server status
+	 */
 	public boolean getServerStatus() {
 		return serverStatus;
 	}
 	
-    // default send method used to send game state updates to the game host.
-	// NOTE: make sure to call getServerStatus() whenever send() is called
+    	/**
+    	 *  Default send method used to send game state updates to the game host.
+	 */
 	@Override
 	public boolean send(GameState g) {
 		if (! ct.send(g)) {
 			serverStatus = false;
 			this.shutDown();
 			return false;
-			
 		}
-		
 		return true;
 	}
-
+	
+	/**
+	 *  Notifes the Game when new GameState update arrives.
+	 */
 	@Override
 	public void receive(GameState fromServer) {
 		G.doReceiveGameState(fromServer);
 	}
 	
-	// send an initial game state to the server to register the client
-	// also starts the client side comms task to listen to incoming game states
+	/** Sends an initial GameState to the server to register the client also 
+	 *  starts the client side comms task to listen to incoming GameStates.
+	 */
     public void init() {
     	GameState initState = new GameState();
-    	
+    	// initial game state will always have it's id set to -1.
     	initState.setID(-1);
     	
     	try {
-    		// send initial game state to the server
+    		// sends the initial game state to the server
     		ObjectOutputStream oos = new ObjectOutputStream(outGoingSocket.getOutputStream());
     		oos.writeObject(initState);
 			oos.flush();
@@ -91,6 +107,10 @@ public class ClientManager extends GameSessionManager {
     	}
     }
     
+    /**
+     *  Cancels the client comms task and close the client socket
+     */
+    @Override
     public void shutDown() {
     	try {
     		ct.cancel(true);
