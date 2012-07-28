7168e52adb6d99f75c4cbf9b4b2026e9cdfe6d5f
Linus Shu <linusshinopy@gmail.com>
2012-07-27 21:12:20 -0300

    Update src/com/cs446/purfect_litter/SessionManager/GameSessionManager.java

--- src/com/cs446/purfect_litter/SessionManager/GameSessionManager.java
+++ src/com/cs446/purfect_litter/SessionManager/GameSessionManager.java
@@ -3,7 +3,10 @@ package com.cs446.purfect_litter.SessionManager;
 import com.cs446.purfect_litter.LogicManager.GameState;
 
 
-
+/**
+ * Responsible for sending/receiving GameState updates to/from the remote player.
+ * Provides an abstraction for ServerManager and ClientManager
+ */
 public abstract class GameSessionManager {
 	
 	//called to send game state updates to default targets
