b998350613ba53e09ed5b7cfb6e425233b9c8060
Linus Shu <linusshinopy@gmail.com>
2012-07-27 22:03:30 -0300

    added comments in server manager

--- src/com/cs446/purfect_litter/SessionManager/ServerManager.java
+++ src/com/cs446/purfect_litter/SessionManager/ServerManager.java
@@ -7,7 +7,10 @@ import java.util.Random;
 import com.cs446.purfect_litter.LogicManager.Game;
 import com.cs446.purfect_litter.LogicManager.GameState;
 
-
+/**
+ *  Responsible for managing server-side communications.
+ */
+ 
 public class ServerManager extends GameSessionManager{
 	List<ServerCommsTask> clients = new ArrayList<ServerCommsTask>();
 	List<Integer> clientIDs = new ArrayList<Integer>(); 
@@ -15,21 +18,27 @@ public class ServerManager extends GameSessionManager{
 	Game G;
 	Random ran = new Random();
 	ServerListenTask lt;
-	
+	/** 
+	 * Constructs a ServerManager, as well as creating a ServerListenTask that listens
+	 * to new ciient connections.
+	 */
 	public ServerManager(Game g) {
 		this.G = g;
-		
+
 		// add server as the first 
 		lt = new ServerListenTask(this);
-	    playerNames.add("Host");
+	    	playerNames.add("Host");
 		lt.execute();
 	}
 	
-	
+	/**
+	 *  Generates a random id number for the connected client, and adds this id to the 
+	 *  clientID list and playername list.
+	 */
 	public boolean addClient(int id) {
 		// NOTE:
 		// hard-coded so that only on player can be registered to the server
-		// change to if (clientIDs.size() < 3) when need to support 3 other clients
+		// change to "if (clientIDs.size() < 3)" when need to support 3 other clients
 		if (clientIDs.isEmpty() && id == -1) {
 			    int assignID = ran.nextInt(1000);
 			    clientIDs.add(Integer.valueOf(assignID));
@@ -40,18 +49,26 @@ public class ServerManager extends GameSessionManager{
 		return false;
 	}
 	
+	/**
+	 * Adds the ServerClientTask associated with the connected client to the clients list.
+	 */
 	public void addClientTask(ServerCommsTask ct) {
 		clients.add(ct);
-	    G.createLogicForServer(playerNames);
-	    receive(G.getGameState());
+	    	G.createLogicForServer(playerNames);
+	    	receive(G.getGameState());
 	}
 	
+	/**
+	 *  Returns the client's id in the clientId list at pos.
+	 */
 	public int getClientId(int pos) {
 		return clientIDs.get(pos).intValue();
 	}
 
-	// default send method used to send game state updates to all the clients currently have their
-	// address registered in clientAddr
+	/**
+	 * Default send method used to broadcast game state updates to all the clients currently have their
+	 * registered in the clients list.
+	 */
 	@Override
 	public boolean send(GameState g) {
 		for (ServerCommsTask ct : clients) {
@@ -71,19 +88,27 @@ public class ServerManager extends GameSessionManager{
 		return true;
 	}
 	
+	/**
+	 *  Notifies the GameFlowManager facde (aka Game) when received a GameState update 
+	 *  from the client.
+	 */
 	@Override
 	public void receive(GameState fromClient) {
 		G.doReceiveGameState(fromClient);
 	}
 	
-	// send game state updates to a specific player
-	// Note: int player = [0,2]
+	/** Sends the GameState update to a specific player; Only used for testing purposes.
+	 * Note: int player = [0,2]
+	 */
 	public void sendTo(GameState g, int player) {
 	    clients.get(player).send(g);
 	}
-
-
-    public void shutDown() {
+	
+	/**
+	 * Cancels the ServerListenTask and shuts down the application.
+	 */
+	@Overrie
+    	public void shutDown() {
     	try {
     		// cancel the ServerListenTask
     		lt.cancel(true);
